0e558ad03fef9882dbc12ff09c4e608cde463488
chylomicronman <bmccann36@gmail.com>
2020-05-21 16:32:47 -0400

    logger

--- src/main/java/com/mercedes/fuelsdk/Scratch.java
+++ src/main/java/com/mercedes/fuelsdk/Scratch.java
@@ -1,11 +1,12 @@
 package com.mercedes.fuelsdk;
 
 import com.exacttarget.fuelsdk.*;
+import org.apache.log4j.Logger;
 
 public class Scratch {
-
+    private static Logger logger = Logger.getLogger(Scratch.class);
     public static void main(String[] args) throws ETSdkException {
-
+        logger.info("hi from logger");
         System.out.println("hello from scratch¡¡\n");
 
     }
